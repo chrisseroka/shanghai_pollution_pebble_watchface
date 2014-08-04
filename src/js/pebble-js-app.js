0e8615f3001a8c73360cc857d6162c2833390eb4
Lee Turner <lvturner@me.com>
2014-08-04 16:03:05 +0800

    Support for configuration and different cities

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,12 +1,12 @@
+var city = 'beijing';
 function fetch_pollution_data(pos) {
   var req = new XMLHttpRequest(),
   version = Date.now(),
-  token = 'XAs1txvhW9Y3rmxqK4zA',
-  city = 'shanghai';
+  token = 'XAs1txvhW9Y3rmxqK4zA';
   req.open('GET', 'http://www.pm25.in/api/querys/pm2_5.json?city='+city+'&token='+token+'&stations=no', true);
 
   req.onload = function(e) {
-  	if (req.readyState == 4 && req.status == 200) {
+    if (req.readyState == 4 && req.status == 200) {
       if (req.status == 200) {
         var response = JSON.parse(req.responseText);
         console.log('response received:');
@@ -17,19 +17,26 @@ function fetch_pollution_data(pos) {
         console.log('pm25:'+pm25);
         Pebble.sendAppMessage({"pm25": ""+pm25+"","pm2524h":""+pm2524h+"","aqi":""+aqi+""});
       } else {
-      	console.log("Error");
+        console.log("Error");
       }
     }
-  }
+  };
   req.send(null);
 }
 
 Pebble.addEventListener("ready", function(e) {
-  console.log('Starting Shanghai Pollution Watchface');
   fetch_pollution_data();
 });
 
-
 Pebble.addEventListener("appmessage", function(e) {
   fetch_pollution_data();
+});
+
+Pebble.addEventListener("showConfiguration", function(e) {
+  Pebble.openURL('https://raw.githubusercontent.com/lvx3/shanghai_pollution_pebble_watchface/master/configurable.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  var options = JSON.parse(decodeURIComponent(e.response));
+  city = options.city;
 });
